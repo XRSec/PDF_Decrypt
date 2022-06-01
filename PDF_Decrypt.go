51ccd33d32308f06c614ddaee519416fda65682f
星冉 <troy@zygd.site>
2022-06-02 01:52:21 +0800

    Automatically clear empty directories :memo:

--- PDF_Decrypt.go
+++ PDF_Decrypt.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"os"
 	"path"
@@ -50,8 +51,8 @@ func adsPath(folder string) string {
 	return adspath
 }
 
-func inputFolders(folder string) []string {
-	if err := filepath.Walk(adsPath(folder), func(path string, info os.FileInfo, err error) error {
+func inputFolders(filePath string) []string {
+	if err := filepath.Walk(adsPath(filePath), func(path string, info os.FileInfo, err error) error {
 		if filepath.Ext(path) == ".pdf" {
 			fileList = append(fileList, path)
 		}
@@ -77,14 +78,41 @@ func inputFolders(folder string) []string {
 	return fileList
 }
 
-func outputFolders(outputFolders string) {
-	if _, err := os.Stat(adsPath(outputFolders)); err != nil {
+func outPutFolder(filePath string) {
+	filePath = adsPath(filePath)
+	if _, err := os.Stat(filePath); err != nil {
 		if !os.IsExist(err) {
-			err := os.Mkdir(adsPath(outputFolders), 0755)
+			err := os.Mkdir(filePath, 0755)
 			if err != nil {
 				_, _ = color.New(color.FgYellow).Println(err)
 				os.Exit(1)
 			}
+		}
+	}
+}
+
+func outPutFolderClean(filePath string) {
+	var (
+		files []fs.FileInfo
+		err   error
+	)
+	filePath = adsPath(filePath)
+	if files, err = ioutil.ReadDir(filePath); err != nil {
+		_, _ = color.New(color.FgYellow).Println(err)
+		os.Exit(1)
+	}
+	if len(files) == 0 {
+		if err := os.Remove(filePath); err != nil {
+			_, _ = color.New(color.FgYellow).Println(err)
+			os.Exit(1)
+		}
+		_, _ = color.New(color.FgRed).Printf(" Remove Folder: ")
+		_, _ = color.New(color.FgWhite).Println(filePath)
+		return
+	}
+	for _, v := range files {
+		if v.IsDir() {
+			outPutFolderClean(path.Join(filePath, v.Name()))
 		}
 	}
 }
@@ -100,8 +128,9 @@ func Decrypt(fileList []string) {
 
 }
 
-func DecryptFile(inFileName, p string) bool {
+func DecryptFile(filePath, p string) bool {
 	//conf.UserPW = i
+	filePath = adsPath(filePath)
 	if !pdfcpu.MemberOf(pdfcpu.ConfigPath, []string{"default", "disable"}) {
 		if err := pdfcpu.EnsureDefaultConfigAt(pdfcpu.ConfigPath); err != nil {
 			_, _ = color.New(color.FgYellow).Println(err)
@@ -110,15 +139,15 @@ func DecryptFile(inFileName, p string) bool {
 	}
 	conf := pdfcpu.NewDefaultConfiguration()
 	conf.OwnerPW = p
-	_, _ = color.New(color.FgCyan).Printf("Decrypting: ")
-	_, _ = color.New(color.FgWhite).Printf("%v %v", path.Base(inFileName), " ")
-	outFileRouter, _ := filepath.Rel(adsPath(inputFolder), inFileName)
+	_, _ = color.New(color.FgCyan).Printf(" Decrypting: ")
+	_, _ = color.New(color.FgWhite).Printf(path.Base(filePath))
+	outFileRouter, _ := filepath.Rel(adsPath(inputFolder), filePath)
 	outFilePath := path.Join(adsPath(outputFolder), outFileRouter)
-	if err := api.DecryptFile(inFileName, outFilePath, conf); err != nil {
+	if err := api.DecryptFile(filePath, outFilePath, conf); err != nil {
 		if strings.Contains(err.Error(), "not encrypted") {
 			_, _ = color.New(color.FgGreen).Println("Not encrypted")
 			var l3 []byte
-			if l3, err = ioutil.ReadFile(inFileName); err != nil {
+			if l3, err = ioutil.ReadFile(filePath); err != nil {
 				_, _ = color.New(color.FgYellow).Println(err)
 				os.Exit(1)
 			}
@@ -131,29 +160,30 @@ func DecryptFile(inFileName, p string) bool {
 		_, _ = color.New(color.FgYellow).Println(err)
 		return false
 	}
-	_, _ = color.New(color.FgGreen).Println("Decrypted! PassWord: ", p)
+	_, _ = color.New(color.FgGreen).Printf(" Decrypted!")
+	_, _ = color.New(color.FgWhite).Println(" PassWord:", p)
 	return true
 }
 
 func main() {
+	_, _ = color.New(color.FgMagenta).Println("\n --------------------------------------")
 	Flag()
 	flag.Parse()
-	if help {
+	if help || len(os.Args) == 1 {
 		fmt.Println(` Usage of PDF_Decrypt:
-  -help -h
-        Display help information
-  -in string
-        in explorer
-  -out string
-        out explorer
-  -pass string
-        password (abc | abc\\def\\ghi)
-  -version -v
-        PDF_Decrypt version`)
+   -help -h
+         Display help information
+   -in string
+         in explorer
+   -out string
+         out explorer
+   -pass string
+         password (abc | abc\\def\\ghi)
+   -version -v
+         PDF_Decrypt version`, "\n")
 		return
 	}
 	if version {
-		_, _ = color.New(color.FgMagenta).Println(" --------------------------------------")
 		_, _ = color.New(color.FgMagenta).Println(" |  Version:", versionData)
 		_, _ = color.New(color.FgMagenta).Println(" |  BuildTime:", buildTime)
 		_, _ = color.New(color.FgMagenta).Println(" |  Author:", author)
@@ -161,7 +191,11 @@ func main() {
 		_, _ = color.New(color.FgMagenta).Println(" --------------------------------------")
 		return
 	}
-	outputFolders(outputFolder)
+	_, _ = color.New(color.FgMagenta).Println(" Start PDF_Decrypt ...")
+	outPutFolder(outputFolder)
 	Decrypt(inputFolders(inputFolder))
-	_, _ = color.New(color.FgGreen).Printf("\nDecrypted %v File\n", len(fileList))
+	for i := 0; i < 7; i++ {
+		outPutFolderClean(outputFolder)
+	}
+	_, _ = color.New(color.FgGreen).Printf("\n Decrypted %v File\n", len(fileList))
 }
